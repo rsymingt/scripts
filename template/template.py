078a1e461f9149b48db7afb74a6e3456569272f8
Ryan <rsymingt@uoguelph.ca>
2018-11-15 18:33:35 -0500

    "update template"

--- template/template.py
+++ template/template.py
@@ -3,9 +3,11 @@
 import sys
 import os
 
+loc = ""
+
 def main(command):
     try:
-        with open("templates/" + command) as file:
+        with open(loc + "templates/" + command) as file:
             print(file.read())
     except(IOError) as e:
         print("template does not exist")
@@ -13,9 +15,16 @@ def main(command):
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         sys.exit("please enter template name or --help")
-    command = sys.argv[1]
-    if command == "--help":
-        files = os.listdir("templates")
+
+    command = 0
+    if len(sys.argv) >= 3:
+        command = sys.argv[2]
+        loc = sys.argv[1]
+    else:
+        command = sys.argv[1]
+
+    if command == "-h":
+        files = os.listdir(loc + "templates")
         print("TEMPLATES\n---------")
         for file in files:
             print(file)
