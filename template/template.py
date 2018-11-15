f2ddd8a3d616863961ca9c733c50d6b56d8a1069
rsymingt <rsymingt@uoguelph.ca>
2018-11-15 17:38:36 -0500

    "added template script "

--- template/template.py
+++ template/template.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
 import sys
+import os
 
-def main():
-    print(sys.argv)
+def main(command):
+    try:
+        with open("templates/" + command) as file:
+            print(file.read())
+    except(IOError) as e:
+        print("template does not exist")
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) <= 1:
+        sys.exit("please enter template name or --help")
+    command = sys.argv[1]
+    if command == "--help":
+        files = os.listdir("templates")
+        print("TEMPLATES\n---------")
+        for file in files:
+            print(file)
+    else:
+        main(command)
