a3c475225fcca4c6870fa12a81c2a428f3379196
Ryan <rsymingt@uoguelph.ca>
2018-11-15 18:08:15 -0500

    "test"

--- git-save.py
+++ git-save.py
@@ -7,9 +7,9 @@ import json
 from os.path import expanduser
 
 credentials = expanduser("~") + "/.credentials"
-commands = ['-s', '-p', '-h']
 
-def save(username, password, commit_msg):
+def save(username, password):
+    commit_msg = input("enter in commit: ")
     current_folder = os.path.relpath('.', '..')
     url = "https://" + username + ":" + password + "@github.com/" + username + "/" + current_folder + ".git"
 
@@ -31,40 +31,41 @@ def save(username, password, commit_msg):
     process = subprocess.Popen(command.split())
     output,error = process.communicate()
 
+def pull():
+    print("test")
+
+def help():
+    print("test")
+
+commands = {'-s': save, '-p': pull, '-h': help}
+
 def main():
     is_cred = os.path.isfile(credentials)
-    command = false
+    command = 0
 
     if len(sys.argv) < 2:
         sys.exit('please enter <option> <arguments>')
 
     command = sys.argv[1]
     if command not in commands:
-        sys.exit(command + " does not exist, type -h")
+        sys.exit("\"" + command + "\" does not exist, type -h for list of commands")
 
-
-    # if os.path.isfile(credentials):
-    #     if(len(sys.argv) >= 2):
-    #         commit_msg = sys.argv[1]
-    #         with open(credentials) as json_data:
-    #             creds = json.load(json_data)
-    #             if all (key in creds for key in ("username", "password")):
-    #                 username, password = creds['username'], creds['password']
-    #             else:
-    #                 print("error in creds in ~/.credentials")
-    #         save(username, password, commit_msg)
-    #     else:
-    #         print("please enter <commit name>")
-    # else:
-    #     if(len(sys.argv) >= 4):
-    #         username = sys.argv[1]
-    #         password = sys.argv[2]
-    #         commit_msg = sys.argv[3]
-    #         save(username, password, commit_msg)
-    #     else:
-    #         print("please enter <username> <password> <commit name>")
-
-
+    if is_cred:
+        with open(credentials) as json_data:
+            creds = json.load(json_data)
+            if all (key in creds for key in ("username", "password")):
+                username, password = creds['username'], creds['password']
+            else:
+                print("error in creds in ~/.credentials")
+        save(username, password)
+    else:
+        if(len(sys.argv) >= 4):
+            username = sys.argv[1]
+            password = sys.argv[2]
+            commit_msg = sys.argv[3]
+            # save(username, password, commit_msg)
+        else:
+            print("please enter <username> <password>")
 
 if __name__ == '__main__':
     main();
