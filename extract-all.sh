6545ffdb09ef20c4f537683fe09334f616b1e517
ryan <ryan@home>
2022-12-23 18:10:26 -0500

    extract script

---
+++ extract-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+delete=false
+
+while getopts d flag
+do
+    case "${flag}" in
+        d) delete=true;;
+    esac
+done
+
+echo $delete
+
+shopt -s globstar
+for f in **/*.rar; do
+    extractFolder="${f%.*}"
+    mkdir $extractFolder
+
+    unrar e -o+ "$f" $extractFolder
+
+    if [ $? -eq 0 ]; then
+        echo 'success'
+        if [ $delete = true ]; then
+            find $(dirname $f) -maxdepth 1 -type f -delete
+        fi
+    fi
+
+done
