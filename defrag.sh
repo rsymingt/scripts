62189576b7255a443f89c2fc87a3e9a6818c1144
Ryan <ryanwsymington@gmail.com>
2024-09-18 13:13:47 -0600

    changes

---
+++ defrag.sh
@@ -0,0 +1,6 @@
+
+# info about raid array
+# adadm -D /dev/md0
+
+# scan for fragmentation
+# e4defrag -c /mnt/raid
--- extract-all.sh
+++ extract-all.sh
@@ -16,12 +16,13 @@ for f in **/*.rar; do
     extractFolder="${f%.*}"
     mkdir "$extractFolder"
 
-    unrar e -o- "$f" "$extractFolder"
+    unrar e -o+ "$f" "$extractFolder"
 
     if [ $? -eq 0 ]; then
         echo 'success'
         if [ $delete = true ]; then
             find "$(dirname "$f")" -maxdepth 1 -type f -delete
+            # find -regex ".*\.\(r.*\|rar\)" -type f -delete
         fi
     fi
 
